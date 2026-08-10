57862e7c6df48a979af613ed8520ce65df1ba94c
dimto13 <93082815+dimto13@users.noreply.github.com>
2026-08-10 15:46:51 +0200

    AITP-81: export engine contracts

--- packages/training-engine/src/index.ts
+++ packages/training-engine/src/index.ts
@@ -1,2 +1,6 @@
 export * from "./types.ts";
+export * from "./contentHierarchy.ts";
+export * from "./eventBus.ts";
+export * from "./stateMachine.ts";
 export * from "./trainingProgress.ts";
+export * from "./validation.ts";
