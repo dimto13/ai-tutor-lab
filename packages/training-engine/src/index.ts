6125c1ae39fc943f10b4c51d0e0c456a18e4c6f7
dimto13 <93082815+dimto13@users.noreply.github.com>
2026-08-11 21:43:32 +0200

    feat: export training state repository contract

--- packages/training-engine/src/index.ts
+++ packages/training-engine/src/index.ts
@@ -2,6 +2,7 @@ export * from "./types.ts";
 export * from "./contentHierarchy.ts";
 export * from "./eventBus.ts";
 export * from "./helpPolicy.ts";
+export * from "./persistence.ts";
 export * from "./stateMachine.ts";
 export * from "./trainingProgress.ts";
 export * from "./validation.ts";
