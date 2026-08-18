fb353ff348daf921f0e6a4139589bbe07e151a68
dimto13 <93082815+dimto13@users.noreply.github.com>
2026-08-19 00:01:21 +0200

    test: add telemetry deletion worker behavior test shell

---
+++ tests/architecture/telemetryDeletionWorkerBehavior.test.ts
@@ -0,0 +1,7 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import { createTelemetryDeletionHandler } from "../../amplify/functions/telemetry-deletion-worker/handler.js";
+
+void assert;
+void test;
+void createTelemetryDeletionHandler;
